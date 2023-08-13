cf78a49ea184d075284e09e68c4ad97753fad51b
Guilherme Marques <guilhermepereiramarques@hotmail.com>
2023-08-12 22:18:27 -0300

    FIX - Fixed CategoryForm without a RequestBody on Swagger

--- DTOs/CategoryForm.cs
+++ DTOs/CategoryForm.cs
@@ -2,6 +2,6 @@ namespace CatalogAPI.Entities.DTOs;
 
 public class CategoryForm
 {
-    public string? Name { get; private set; }
-    public string? ImageUrl { get; private set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
 }
